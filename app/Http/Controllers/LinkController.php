38aacf7828a0e75f54dc237188eee777d129a03f
Tejkerkoks <banaskarol04@gmail.com>
2024-12-02 13:44:28 +0100

    Dodano obsługe do przekierowywania skróconego linku

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,39 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Link;
 use Illuminate\Http\Request;
+use App\Models\Link;  
 
 class LinkController extends Controller
 {
-    // Metoda do wyświetlania wszystkich skróconych linków
-    public function index()
-    {
-        // Pobieranie wszystkich linków z bazy danych
-        $links = Link::all();
-        
-        // Zwracanie widoku z danymi linków
-        return view('links.index', compact('links'));
-    }
-
-    // Metoda do tworzenia nowego skróconego linku
+    // Metoda do tworzenia nowego linku
     public function store(Request $request)
     {
-        // Walidacja wprowadzonego URL
+        // Walidacja wejściowego URL
         $request->validate([
             'original_url' => 'required|url',
         ]);
 
-        // Generowanie unikalnego skróconego linku
-        $shortenedUrl = substr(md5(time()), 0, 6); // Przykład prostego skrótu
+        // Generowanie skróconego URL
+        $shortenedUrl = substr(md5(uniqid(rand(), true)), 0, 6);
 
-        // Tworzenie nowego rekordu w tabeli 'links'
-        Link::create([
-            'original_url' => $request->original_url,
-            'shortened_url' => $shortenedUrl,
+        // Tworzenie nowego wpisu w bazie danych
+        $link = new Link();
+        $link->original_url = $request->original_url;
+        $link->shortened_url = $shortenedUrl;
+        $link->save();
+
+        return response()->json([
+            'original_url' => $link->original_url,
+            'shortened_url' => $link->shortened_url,
         ]);
+    }
 
-        // Przekierowanie do strony z listą linków
-        return redirect()->route('links.index');
+    // Metoda do przekierowywania użytkownika z krótkiego URL do pełnego
+    public function redirect($shortUrl)
+    {
+        // Wyszukaj link w bazie danych na podstawie skróconego URL
+        $link = Link::where('shortened_url', $shortUrl)->firstOrFail();
+
+        // Przekierowanie do oryginalnego URL
+        return redirect($link->original_url);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LinkController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{shortUrl}', [LinkController::class, 'redirect']);
